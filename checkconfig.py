a2807ee020a64a57a0eecc2d4c0f752eb3de9fc6
xiaodongpan <xdongp@gmail.com>
2018-09-17 00:49:31 +0800

    update

---
+++ checkconfig.py
@@ -0,0 +1,64 @@
+import os
+import json
+import urllib2
+
+URL = "http://adp.brocdn.com:9999/netads/api/getInterceptRules?from=527691484216954880"
+OUT = "conf/out.ini"
+CACHE = "conf/.cache"
+
+def read_cache():
+    if os.path.exists(CACHE):
+        fd = open(CACHE)
+        data = fd.read()
+        fd.close()
+        if not data:
+            return 0
+        return int(data)
+    else:
+        return 0
+        
+
+def save_cache(length):
+    fd = open(CACHE, "w")
+    fd.write(str(length))
+    fd.flush()
+    fd.close()
+
+def reload():
+    cmd = "killall -USR2 fastj"
+    os.system()
+
+def check(url):
+    cmd = "curl -s -I %s|grep Length" % url
+    fd = os.popen(cmd)
+    data = fd.read()
+    fd.close()
+    length = data.split(":")[1].strip()
+    if length == 0:
+        print "config length is 0, return"
+        return
+	#length = int(length)
+    old_length = read_cache()
+    if int(old_length) != int(length):
+        print "config changed ,load it... "
+        parse(url)
+        print "parse ok , reload it to fastj... "
+        #reload it
+        print "load it ok, save cache"
+        save_cache(length)
+    else:
+        print "config have not changed"
+    
+def parse(url):
+    url_fd = urllib2.urlopen(url)
+    data = url_fd.read()
+    url_fd.close()
+    lst = json.loads(data)
+    fd = open(OUT, "w")
+    for e in lst:
+        fd.write("%s|%s|%s|%s|%s|%s\n" % (e['chance'], e['match'], e['platform'], e['domain'], e['path'], e['redirect']))
+    fd.flush()
+    fd.close()
+ 
+if __name__ == "__main__":
+    check(URL)
---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup ./fastj p2p2 em1 F8:BC:12:38:A7:BC 4c:b1:6c:8b:8f:39 &> /home/fastj/fastj.log &
